13ecab6c218ab782daca235500817f1e34f7ba09
dhhan <hellotect2022@gmail.com>
2024-02-02 13:05:31 +0900

    파일 쓰기전에 기존파일 삭제 로직추가

--- test_graph/main.py
+++ test_graph/main.py
@@ -2,29 +2,30 @@ import matplotlib.pyplot as plt
 import re
 import matplotlib.dates as mdates
 from matplotlib.ticker import AutoLocator
+import os
 
 file_path = './record.txt'
 
+x_tmp=[]
+y_tmp=[]
+
 x_data=[]
 y_data=[]
-
-def read_file():
-    with open(file_path,'r') as file:
-        for line in file:
-            #print(match_re(r'\[([0-9]+)\]',line.strip()))
-            x_date.append(match_re(r'\[(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2})\]',line.strip()))
-            y_count.append(match_re(r'\[([0-9]+)\]',line.strip()))
-            #print(line.strip())
-            with open('./write.txt','a') as file:
-                file.write(match_re(r'\[(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2})\]',line.strip())+"|"+match_re(r'\[([0-9]+)\]',line.strip())+"\n")
-            #print(match_re(r'\[(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2})\]',line.strip())+"|"+match_re(r'\[([0-9]+)\]',line.strip()))
-    
-    #paint_plot(x_date,y_count,"fuck")
 
 def match_re(pattern,text):
     match = re.search(pattern,text)
     if match:
         return match.group(1)
+
+def read_file():
+    if os.path.exists("./write.txt"):
+        os.remove("./write.txt")
+    with open(file_path,'r') as file:
+        for line in file:
+            x_tmp.append(match_re(r'\[(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2})\]',line.strip()))
+            y_tmp.append(match_re(r'\[([0-9]+)\]',line.strip()))
+            with open('./write.txt','a') as file:
+                file.write(match_re(r'\[(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2})\]',line.strip())+"|"+match_re(r'\[([0-9]+)\]',line.strip())+"\n")
 
 def paint_plot():
     # |를 기준으로 문자열을 나누기
@@ -34,10 +35,10 @@ def paint_plot():
             x_data.append(x_str)
             y_data.append(int(y_str))
     # 선 그래프 그리기
-    #plt.plot(x_date, y_count, marker='o', linestyle='-', color='b', label='test')
+
     plt.plot(x_data, y_data, linestyle='-', color='b', label='test')
     # 그래프에 제목과 축 레이블 추가
-    plt.title('test1')
+    plt.title("Hoban worker thread")
     plt.xlabel('X data')
     plt.ylabel('Y data')
     # 범례 추가
@@ -58,6 +59,7 @@ def paint_plot():
     # 그래프 표시
     plt.show()
 
+read_file()
 paint_plot()
 
-#read_file()
+#
